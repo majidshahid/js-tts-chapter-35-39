65cef8e7ee6756d41eef3711213d697dfaea3723
majid shahid <majidshahid474@gmail.com>
2021-05-31 15:44:25 -0700

    js tts chapter 31-34

---
+++ app.js
@@ -0,0 +1,95 @@
+// function date (){
+//     document.write(new Date());
+// }
+// date()
+
+
+
+// function fullName (){
+//     var firstName =prompt("Enter your first name ","Majid");
+//     var lastName = prompt("Enter your last name ","Shahid");
+//     var fullName= firstName+" "+ lastName;
+//     document.write("Welcome " + fullName)
+
+// }
+// fullName();
+
+// function add (){
+//     var one = +prompt("Enter first value","30");
+//     var second = +prompt("Enter second value","10000");
+//     var add = one + second;
+//     document.write(add)
+// }
+// add();
+
+
+// function calculater (){
+//     var a= +prompt("enter first value ","22");
+//     var b = +prompt("Enter your second value","40");
+//     var c =prompt("diffiend the operater +,-,/,x","x")
+//     if (c === "+"){
+//         document.write(a+b)
+//     }else if (c == "-"){
+//         document.write(a-b)
+//     }else if (c == "/"){
+//         document.write(a/b)
+//     }else if (c == "x"){
+//         document.write(a*b)
+//     }
+// }
+// calculater()
+
+
+// function squares(){
+//     var a = +prompt("enter value for squares","2");
+//     for (i=0;i<=a;i++){
+//     var b = a*i
+//     }
+//     document.write(b)
+// }
+// squares()
+
+
+// function factor(){
+//     var a =+prompt("enter value ");
+//     for (i=1;i<=a;i++){
+//         document.write(a*i)
+//     }
+// }
+// factor()
+
+// function count (){
+//     var a = +prompt("enter your first value","1");
+//     var b = +prompt("enter your second value ","5");
+//     for (i=a;i<=b;i++){
+//         var c =+ i
+//         document.write(c +"<br>")
+//     }
+// }
+// count()
+
+
+// function area(){
+//     var a = + prompt("Enter width of rectangle")
+//     var b = +  prompt("Enter height of rectangle")
+
+//     document.write(" area of rectangle "+ (a+b) + "<br>");
+//     document.write(" perimeter of rectangle "+2*(a+b) + "<br>");
+// }
+// area()
+
+
+// function palindrome(){
+//     var a = prompt("enter a word ");
+//     for (i=a.length-1;i>=0;i--){
+//         var b = a[i]
+//         if(a==b){
+//             console.log("ok")
+//         }else{
+//             console.log("not ok")
+//         }
+//     }
+  
+    
+// }
+// palindrome()
